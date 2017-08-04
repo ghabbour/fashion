092c797c9759e94e2ac3718a08aa8949ac5d4d80
ghabbour <mohamedghabbour23@gmail.com>
2017-08-04 11:47:12 +0200

    all code

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+
+  $('.menu-btn').click(function(){
+    $('.navmenu').slideToggle();
+  });
+
+  // Initialize smooth smooth-scroll
+  smoothScroll.init({
+    speed: 800
+  });
+
+  // Nice scroll
+  $('html').niceScroll({
+    cursorcolor:'#8e44ad',
+    cursorwidth: "11px",
+    background:"#f5f5f5",
+  });
+
+
+  // tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+
+  // scroll-to-top
+  var scrollButton = $('#scroll-top');
+
+  $(window).scroll(function(){
+    $(this).scrollTop() >= 600 ? scrollButton.show() : scrollButton.hide();
+  });
+
+  scrollButton.click(function(){
+    $('html,body').animate({
+      scrollTop: 0
+    }, 600 );
+  });
+
+});
